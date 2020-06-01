518fa846a1b95b3b382b9385336dcbc54d566ef6
sapirg50 <sapirg50@users.noreply.github.com>
2020-06-01 06:26:28 +0200

    designHTML is DONE!!

--- FlightControlWeb/Controllers/Implementations/FlightsController.cs
+++ FlightControlWeb/Controllers/Implementations/FlightsController.cs
@@ -65,7 +65,7 @@
         // GET: api/Flights?relativeTo=<DateTime>&sync_all
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Flight>>> GetFlightsRelativeTo([FromQuery(Name = "relative_to")] DateTime relativeTo) {
-            if (relativeTo.Kind != DateTimeKind.Utc || true) {
+            if (relativeTo.Kind != DateTimeKind.Utc) {
                 return this.BadRequest($"requset:\"{this.Request}\"\nstatus:\"failed\"\nreason:\"relativeTo format must be yyyy-MM-ddTHH:mm:ssZ (UTC time)\"");
             }
 
